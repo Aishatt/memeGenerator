b429187a2de81ef2fdfc722bcef7d445f71de17d
Adebayo Aishat <aishatadebayor0@gmail.com>
2023-09-16 23:26:11 +0100

    edited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import GetMeme from './meme';
+
+//Nav
 function Nav(){
   return <nav>
   <div class="logo">
@@ -12,6 +14,27 @@ function Nav(){
 </nav>
 }
 
+//add item to list on click
+function Todotesting(){
+  const thingArray= ["thing1", "thing2"]
+  const thingElement = thingArray.map(thing => <p key= {thing}>{thing}</p>)
+//additem onclick function, add one to thingarray and make it the new thing
+  function addItem(){
+    const newThing = `Thing ${thingArray.length + 1}`
+    thingArray.push(newThing)
+    console.log(thingArray)
+  }
+
+  return(<>
+  <button onClick={addItem} className='button'>Add Item </button>
+    {thingElement}
+
+  </>)
+
+}
+
+
+
 const isGoingOut= true
 let answer = isGoingOut ? 'yes' : 'No'
 
@@ -21,6 +44,7 @@ function App() {
       <Nav />
       <h1>{answer}</h1>
       <GetMeme />
+      <Todotesting />
     </main>
   );
 }
